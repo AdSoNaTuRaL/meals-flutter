9affbc12d0914a8c203cb3e05abfbfe692fcc73b
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-24 11:02:33 +0100

    Applying styles

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/category_item.dart';
+import 'package:meals/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos cozinhar?'),
       ),
-      children: [],
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((category) {
+          return CategoryItem(category);
+        }).toList(),
+      ),
     );
   }
 }
