d1a67e4ea1bf37349c1f09fd69f821b685731e93
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-24 11:01:55 +0100

    Refactoring main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,33 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Vamos cozinhar?'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Navegar é preciso!',
-            ),
-          ],
-        ),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
