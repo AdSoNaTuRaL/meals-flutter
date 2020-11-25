2daa7b7a749dee96ec9743e89ecb47058a030822
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-25 20:54:06 +0100

    Changing categories meals screen

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:meals/models/category.dart';
+import 'package:meals/components/meal_item.dart';
+import 'package:meals/data/dummy_data.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  final Category category;
-
-  const CategoriesMealsScreen(this.category);
-
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context).settings.arguments as Category;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Receitas'),
       ),
-      body: Center(
-        child: Text('Receitas da categoria ${category.title}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
       ),
     );
   }
