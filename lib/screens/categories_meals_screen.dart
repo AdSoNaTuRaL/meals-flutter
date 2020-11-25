238a498f180ee8bc486bc7cfed5728f2880ad350
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-25 16:12:44 +0100

    Adding new screen

---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  const CategoriesMealsScreen(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Receitas'),
+      ),
+      body: Center(
+        child: Text('Receitas da categoria ${category.title}'),
+      ),
+    );
+  }
+}
