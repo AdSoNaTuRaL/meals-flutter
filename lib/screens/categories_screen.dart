0f632d573d12a8a23442bc3127e64fbaff8a21f5
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-30 18:55:40 +0100

    Updating few parameters

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,22 +5,17 @@ import 'package:meals/data/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Vamos cozinhar?'),
+    return GridView(
+      padding: EdgeInsets.all(20),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 10 / 6,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: EdgeInsets.all(20),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES.map((category) {
-          return CategoryItem(category);
-        }).toList(),
-      ),
+      children: DUMMY_CATEGORIES.map((category) {
+        return CategoryItem(category);
+      }).toList(),
     );
   }
 }
